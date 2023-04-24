47aa1064af72f970177993c14bb51258e4ef8984
Matos <davimatos07@gmail.com>
2023-04-24 17:52:43 -0300

    Terceiro commit

--- src/main/java/br/com/ada/maventeste/musica/controller/MusicaController.java
+++ src/main/java/br/com/ada/maventeste/musica/controller/MusicaController.java
@@ -1,34 +1,41 @@
 package br.com.ada.maventeste.musica.controller;
 
+import br.com.ada.maventeste.musica.dto.MusicaDTO;
+import br.com.ada.maventeste.musica.dto.ResultadoDTO;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 @RestController
 @RequestMapping("/musica")
 public class MusicaController {
 
+    private List<MusicaDTO> musicaDTOs = new ArrayList<>();
+
     @GetMapping("/detalhe")
-    public Map<String, String> detalhe(@RequestParam String nome) {
-        return new HashMap<>() {{
-            put("nome", nome);
-            put("genero", "genero");
-        }};
-    }
+        public List<MusicaDTO> lista(@RequestParam String filter) {
+            List<MusicaDTO> listaFiltrada = new ArrayList<>();
+            for (MusicaDTO musicaDTO : musicaDTOs({
+            if (musicaDTO.setNome().contains(filter)){
+                listaFiltrada.add(musicaDTO);
+            }
+}
+            return listaFiltrada;
+        }
 
     @PostMapping
-    public String criar(@RequestBody Map<String, String> dados) {
+    public ResponseEntity<ResultadoDTO> criar(@RequestBody MusicaDTO musicaDTO) {
+        return ResponseEntity.status(HttpStatus.OK).body(new ResultadoDTO().setResultado(true).setMensagem("funcionou!"));
 
-        Map<String, String> musicas = new HashMap<>();
-
-        musicas.putAll(dados);
-
-        return "Música " + musicas.get("nome") + " criada com sucesso!";
     }
 
     @PutMapping
-    public String editar(@RequestBody Map<String, String> dados) {
+    public ResponseEntity<ResultadoDTO> editar(@RequestBody MusicaDTO musicaDTO) {
 
         Map<String, String> musicas = new HashMap<>();
 
